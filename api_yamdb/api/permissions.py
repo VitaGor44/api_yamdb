f11f10ecb2034777165400540ad2c9a2b4d52e0c
VitaGor44 <95421322+VitaGor44@users.noreply.github.com>
2023-06-15 16:57:39 +0700

    Change after 2 revue and PEP again

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,7 +5,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return request.user.is_authenticated and (
-                    request.user.is_admin or request.user.is_staff
+                request.user.is_admin or request.user.is_staff
             )
 
 
